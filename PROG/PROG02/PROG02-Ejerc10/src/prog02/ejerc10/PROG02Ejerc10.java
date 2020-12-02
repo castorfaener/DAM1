6cdf18c03f06c40ada37e816784a4fa4ad7154bd
castorfaener <castorfaener@gmail.com>
2020-12-02 22:10:42 +0100

    continuo con Ejercicio10 PROG02

--- PROG/PROG02/PROG02-Ejerc10/src/prog02/ejerc10/PROG02Ejerc10.java
+++ PROG/PROG02/PROG02-Ejerc10/src/prog02/ejerc10/PROG02Ejerc10.java
@@ -17,18 +17,23 @@ public class PROG02Ejerc10 {
         byte by = 2;
         byte bz = (byte)(bx - by);
         System.out.println("------- Operaciones con byte -------");
-        System.out.printf("byte : " + bx +" - " + by + " = " + bz);
+        System.out.printf("byte: " + bx +" - " + by + " = " + bz);
         bx = -128;
         by = 1;
         bz = (byte)(bx - by);
         System.out.printf("\nbyte : " + bx +" - " + by + " = " + bz);
-        int bzint = (int)(bx - by);
+        int bzint = (int)(bx - by);                                             
         System.out.printf("\n(int)("+ bx +" - " + by + ") = " + bzint);
         short sx = 5;
         short sy = 2;
         short sz = (short)(sx - sy);
-        
-        
+        System.out.println("\n\n------- Operaciones con short -------");
+        System.out.printf("short: " + sx +" - " + sy + " = " + sz);
+        sx = 32767;
+        sy = 1;
+        sz = (short)(sx + sy);
+        System.out.printf("\nshort: " + sx +" + " + sy + " = " + sz);
+        System.out.println("\n\n------- Operaciones con char -------");
         
         
         
