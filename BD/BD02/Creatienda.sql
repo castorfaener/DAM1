ea3f1d6f18e86d3242e18f8ea316e30f6f691a0f
castorfaener <castorfaener@gmail.com>
2020-12-11 11:08:47 +0100

    Depuramos Creatienda.sql - Ok

--- BD/BD02/Creatienda.sql
+++ BD/BD02/Creatienda.sql
@@ -2,7 +2,7 @@
 --Creamos la tabla FAMILIA
 CREATE TABLE FAMILIA (
 	Codfamilia NUMBER(3) CONSTRAINT fa_co_pk PRIMARY KEY,
-	Denofamilia VARCHAR(50) CONSTRAINT fa_de_nn NOT NULL CONSTRAINT fa_de_un UNIQUE)
+	Denofamilia VARCHAR(50) CONSTRAINT fa_de_nn NOT NULL CONSTRAINT fa_de_un UNIQUE);
 
 --Creamos la tabla PRODUCTO
 CREATE TABLE PRODUCTO (
@@ -12,7 +12,7 @@ CREATE TABLE PRODUCTO (
 	Preciobase NUMBER(8,2) CONSTRAINT pr_pr_nn NOT NULL CONSTRAINT pr_pr_ch CHECK (Preciobase > 0),
 	Porcreposicion NUMBER(3) CONSTRAINT pr_po_ch CHECK (Porcreposicion > 0),
 	UnidadesMinimas NUMBER(4) CONSTRAINT pr_un_nn NOT NULL CONSTRAINT pr_un_ch CHECK (UnidadesMinimas >0),
-	Codfamilia NUMBER(3) CONSTRAINT pr_co_nn NOT NULL CONSTRAINT pr_co_fk REFERENCES FAMILIA(Codfamilia))
+	Codfamilia NUMBER(3) CONSTRAINT pr_co_nn NOT NULL CONSTRAINT pr_co_fk REFERENCES FAMILIA(Codfamilia));
 
 -- Creamos la tabla TIENDA
 CREATE TABLE TIENDA (
@@ -20,4 +20,4 @@ CREATE TABLE TIENDA (
 	Denotienda VARCHAR(20) CONSTRAINT ti_co_nn NOT NULL,
 	Telefono VARCHAR(11),
 	Codigopostal VARCHAR(5) CONSTRAINT ti_cod_nn NOT NULL,
-	Provincia VARCHAR(5) CONSTRAINT ti_pr_nn NOT NULL)
+	Provincia VARCHAR(5) CONSTRAINT ti_pr_nn NOT NULL);
