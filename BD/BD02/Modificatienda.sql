810a25a40634f0216194c9e35450556e96c59d30
Paco <castorfaener@gmail.com>
2020-12-12 09:30:05 +0100

    Depurando Modificatienda.sql

--- BD/BD02/Modificatienda.sql
+++ BD/BD02/Modificatienda.sql
@@ -13,7 +13,7 @@
 
 ALTER TABLE STOCK ADD(
 	FechaUltimaEntrada DATE DEFAULT SYSDATE,
-	Beneficio NUMBER(1) CONSTRAINT st_ben_ch CHECK(Beneficio>0 && Beneficio<=5));
+	Beneficio NUMBER(1) CONSTRAINT st_ben_ch CHECK(Beneficio BETWEEN 1 AND 5));
 
 
 /*	 
@@ -27,13 +27,13 @@ ALTER TABLE STOCK ADD(
         Modificar el tamaño de la columna Denoproducto a 50.
 */
 
-ALTER TABLE PRODUCTO DROP COLUMN (Descripcion);
+ALTER TABLE PRODUCTO DROP COLUMN Descripcion;																-- ERROR No deja eliminarla por permisos de usuario SYS
 
-ALTER TABLE PRODUCTO ADD (
+ALTER TABLE PRODUCTO ADD 
 	perecedero CHAR(1) CONSTRAINT pro_per_ch CHECK(perecedero IN('S','N'));
 
-ALTER TABLE PRODUCTO MODIFY(
-	Denoproducto VARCHAR(50));
+ALTER TABLE PRODUCTO MODIFY
+	Denoproducto VARCHAR(50);
 
 
 /*
@@ -43,8 +43,8 @@ ALTER TABLE PRODUCTO MODIFY(
         Añadir una columna llamada IVA, que represente el porcentaje de IVA y únicamente pueda contener los valores 21,10,ó 4.
 */
 
-ALTER TABLE FAMILIA ADD (
-	IVA NUMBER(2) CONSTRAINT fam_iva_ch CHECK(IVA IN ('4','10','21')));
+ALTER TABLE FAMILIA ADD 
+	IVA NUMBER(2) CONSTRAINT fam_iva_ch CHECK(IVA IN ('4','10','21'));
 
 /*	
 
@@ -53,14 +53,14 @@ ALTER TABLE FAMILIA ADD (
         La empresa desea restringir el número de tiendas con las que trabaja, de forma que no pueda haber más de una tienda en una misma zona (la zona se identifica por el código postal). Definir mediante DDL las restricciones necesarias para que se cumpla en el campo correspondiente..
 */
 
-ALTER TABLE TIENDA ADD CONSTRAINT tie_pos_un UNIQUE;
+ALTER TABLE TIENDA ADD CONSTRAINT tie_pos_un UNIQUE;				--ERROR falta parentesis
 
 
 /*	
 	Apartado B: Renombra la tabla STOCK por PRODXTIENDAS.
 */
 
-RENAME TIENDA TO PRODXTIENDAS;
+RENAME STOCK TO PRODXTIENDAS;
 
 /*
 	Apartado C: Elimina la tabla FAMILIA y su contenido si lo tuviera.
@@ -72,14 +72,14 @@ DROP TABLE FAMILIA CASCADE CONSTRAINTS;
 	Apartado D: Crea un usuario llamado C##INVITADO siguiendo los pasos de la unidad 1 y dale todos los privilegios sobre la tabla PRODUCTO.
 */
 
-CREATE USER INVITADO IDENTIFIED BY MyPassword;
-GRANT ALL PRIVILEGES ON PRODUCTO TO INVITADO;
+CREATE USER C##INVITADO IDENTIFIED BY MyPassword;
+GRANT ALL PRIVILEGES ON PRODUCTO TO C##INVITADO;
 
 
 /*
 	Apartado E: Retira los permisos de modificar la estructura de la tabla y borrar contenido de la tabla PRODUCTO al usuario anterior.
 */
 
-REVOKE ALL PRIVILEGES FROM INVITADO;
-GRANT SELECT,INSERT TO INVITADO;
+REVOKE ALL PRIVILEGES FROM C##INVITADO;
+GRANT (SELECT,INSERT) TO INVITADO;
 
