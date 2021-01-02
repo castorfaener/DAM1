6ec6738782f203253afcdc6657375b7fb9324163
castorfaener <castorfaener@gmail.com>
2021-01-02 20:23:19 +0100

    Empezamos PROG05_Ejerc5

---
+++ PROG/PROG04/PROG04_Ejerc5/src/prog04_ejerc5/PROG04_Ejerc5.java
@@ -0,0 +1,21 @@
+/*
+ * Cuando dividimos un número entre 0 se genera un valor indeterminado. En cualquier lenguaje de programación este tipo de operaciones genera un error de ejecución que debe ser 
+ * controlado desde el código para evitar malas experiencias al usuario. En Java, cuando se produce esta operación se genera la excepción ArithmeticException.
+ * Queremos implementar un programa Java que calcule la división de dos números solicitados por teclado (dividendo y divisor).
+ * El programa solicitará números indefinidamente hasta que los dos números solicitados sean -1. Se debe controlar mediante excepciones que el divisor no sea 0.
+ * En caso de serlo, se mostrará un mensaje por pantalla. También habrá que mostrar por pantalla el número de divisiones calculadas. Utiliza número enteros para las variables.
+ */
+package prog04_ejerc5;
+
+/**
+ *
+ * @author Francisco Alacreu Rosello
+ */
+public class PROG04_Ejerc5 {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
