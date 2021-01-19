2848e3a5a7ab1d2d1c378204968d20ca48061ae4
castorfaener <castorfaener@gmail.com>
2021-01-19 22:19:15 +0100

    Ajustamos ejercicios Tarea PROG04

--- PROG/PROG04/PROG04_Ejerc1/src/prog04_ejerc1/PROG04_Ejerc1.java
+++ PROG/PROG04/PROG04_Ejerc1/src/prog04_ejerc1/PROG04_Ejerc1.java
@@ -6,6 +6,12 @@ package prog04_ejerc1;
 
 import java.util.Scanner;
 
+/**
+ *Muestra, utilizando tres bucles diferentes, la tabla de multiplicar de un numero introducido por teclado. 
+ * 
+ * @author Francisco Alacreu Rosello
+ */
+
 public class PROG04_Ejerc1 {
 
     public static void main(String[] args) {
@@ -14,9 +20,9 @@ public class PROG04_Ejerc1 {
         System.out.println("Introduce un numero del 1 al 30 para mostrar su tabla de multiplicar: ");
         int numero = teclado.nextInt();
 
-        if (numero > 0 && numero < 30) {
+        if (numero > 0 && numero < 30) {        //Verificamos que el numero se encuentre en el rango de 0 a 30
 
-            //multiplicarFor(numero);
+            multiplicarFor(numero);
             multiplicarWhile(numero);
             multiplicarDoWhile(numero);
 
@@ -26,9 +32,13 @@ public class PROG04_Ejerc1 {
         }
 
     }
-/*
-    /**
+
     
+    /**
+     * Devuelve la tabla de multiplicar del numero pasado como parametro utilizando el bucle for
+     * 
+     * @param num numero para calcular la tabla de multiplicar
+     */
     
     static void multiplicarFor(int num) {
 
@@ -37,9 +47,14 @@ public class PROG04_Ejerc1 {
             System.out.println(num + " x " + i + " = " + (num * i));
 
         }
+    }
     
-    */
-        
+    /**
+     * Devuelve la tabla de multiplicar del numero pasado como parametro utilizando el bucle while
+     * 
+     * @param num numero para calcular la tabla de multiplicar
+     */
+    
     static void multiplicarWhile(int num) {
 
         int i = 0;
@@ -51,6 +66,12 @@ public class PROG04_Ejerc1 {
         }
 
     } 
+    
+    /**
+     * Devuelve la tabla de multiplicar del numero pasado como parametro utilizando el bucle do while
+     * 
+     * @param num numero para calcular la tabla de multiplicar
+     */
     
     static void multiplicarDoWhile(int num){
         
