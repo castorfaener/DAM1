199b452417f2edc96da3489da48e00f290f0b2b1
Paco <castorfaener@gmail.com>
2021-02-03 20:40:00 +0100

    Tarea BD02 Finalizada

--- BD/BD02/Creatienda.sql
+++ BD/BD02/Creatienda.sql
@@ -1,30 +1,30 @@
 
 --Creamos la tabla FAMILIA
 CREATE TABLE FAMILIA (
-	Codfamilia NUMBER(3) CONSTRAINT fa_co_pk PRIMARY KEY,
-	Denofamilia VARCHAR(50) CONSTRAINT fa_de_nn NOT NULL CONSTRAINT fa_de_un UNIQUE);
+	Codfamilia NUMBER(3) CONSTRAINT fam_co_pk PRIMARY KEY,
+	Denofamilia VARCHAR(50) CONSTRAINT fam_de_nn NOT NULL CONSTRAINT fam_de_un UNIQUE);
 
 --Creamos la tabla PRODUCTO
 CREATE TABLE PRODUCTO (
-	Codproducto NUMBER(5) CONSTRAINT pr_co_pk PRIMARY KEY,
-	Denoproducto VARCHAR(20) CONSTRAINT pr_de_nn NOT NULL,
+	Codproducto NUMBER(5) CONSTRAINT pro_co_pk PRIMARY KEY,
+	Denoproducto VARCHAR(20) CONSTRAINT pro_de_nn NOT NULL,
 	Descripcion VARCHAR(100),
-	Preciobase NUMBER(8,2) CONSTRAINT pr_pr_nn NOT NULL CONSTRAINT pr_pr_ch CHECK (Preciobase > 0),
-	Porcreposicion NUMBER(3) CONSTRAINT pr_po_ch CHECK (Porcreposicion > 0),
+	PrecioBase NUMBER(8,2) CONSTRAINT pro_pr_nn NOT NULL CONSTRAINT pro_pr_ch CHECK (PrecioBase > 0),
+	PorcReposicion NUMBER(3) CONSTRAINT pro_po_ch CHECK (PorcReposicion > 0),
 	UnidadesMinimas NUMBER(4) CONSTRAINT pr_un_nn NOT NULL CONSTRAINT pr_un_ch CHECK (UnidadesMinimas >0),
-	Codfamilia NUMBER(3) CONSTRAINT pr_co_nn NOT NULL CONSTRAINT pr_co_fk REFERENCES FAMILIA(Codfamilia));
+	Codfamilia NUMBER(3) CONSTRAINT pro_co_nn NOT NULL CONSTRAINT pro_co_fk REFERENCES FAMILIA(Codfamilia));
 
 -- Creamos la tabla TIENDA
 CREATE TABLE TIENDA (
-	Codtienda NUMBER(3) CONSTRAINT ti_co_pk PRIMARY KEY,
-	Denotienda VARCHAR(20) CONSTRAINT ti_co_nn NOT NULL,
+	Codtienda NUMBER(3) CONSTRAINT tie_co_pk PRIMARY KEY,
+	Denotienda VARCHAR(20) CONSTRAINT tie_co_nn NOT NULL,
 	Telefono VARCHAR(11),
-	Codigopostal VARCHAR(5) CONSTRAINT ti_cod_nn NOT NULL,
-	Provincia VARCHAR(5) CONSTRAINT ti_pr_nn NOT NULL);
+	Codigopostal VARCHAR(5) CONSTRAINT tie_cod_nn NOT NULL,
+	Provincia VARCHAR(5) CONSTRAINT tie_pr_nn NOT NULL);
 
 -- Creamos la tabla STOCK
 CREATE TABLE STOCK (
-	Codtienda NUMBER(3) CONSTRAINT st_co_nn NOT NULL CONSTRAINT st_co_fk REFERENCES TIENDA,
-	Codproducto NUMBER(5) CONSTRAINT st_codp_nn NOT NULL CONSTRAINT st_codp_fk REFERENCES PRODUCTO,
-	CONSTRAINT st_co_pk PRIMARY KEY (Codtienda, Codproducto),
-	Unidades NUMBER(6) CONSTRAINT st_un_nn NOT NULL CONSTRAINT st_un_ch CHECK(Unidades >= 0));
+	Codtienda NUMBER(3) CONSTRAINT sto_co_nn NOT NULL CONSTRAINT st_co_fk REFERENCES TIENDA,
+	Codproducto NUMBER(5) CONSTRAINT sto_codp_nn NOT NULL CONSTRAINT sto_codp_fk REFERENCES PRODUCTO,
+	CONSTRAINT sto_co_pk PRIMARY KEY (Codtienda, Codproducto),
+	Unidades NUMBER(6) CONSTRAINT sto_un_nn NOT NULL CONSTRAINT sto_un_ch CHECK(Unidades >= 0));
